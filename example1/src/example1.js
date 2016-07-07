1530945db6b71b2b38773731dcbe893530a104e9
timontang <d.penghai@gmail.com>
2016-07-07 17:36:23 +0800

    no message

--- example1/src/example1.js
+++ example1/src/example1.js
@@ -21,12 +21,50 @@ class App{
 
 		return 'timon app';
 	}
-
-
-
+ 
 }
 
+var appA = new App(null, '19.00');
 
-var app = new App(null, '19.00');
+
+var appB = new class {
+
+	constructor (x, y = '12.00'){
+
+		console.log('constructor (x, '+ y +')');
+	}
+
+}(null, '29.00');
+
+
+class Animal {
+
+	constructor(name){
+
+		this.name = name;
+
+	}
+
+	sleep(){
+		console.log(this.name+'睡觉了');
+	}
+
+	hungry(){
+		console.log(this.name+'饿了');
+	}
+
+	run(){
+		console.log(this.name+'在跑步');
+	}
+
+ }
  
- 
+ class Man extends Animal{
+
+ }
+
+
+ var man = new Man('碧咸');
+
+ man.run();
+ man.hungry();
